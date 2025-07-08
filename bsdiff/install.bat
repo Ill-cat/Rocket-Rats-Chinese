0f64602f2692e9fc9d1b387a31a6ac7e116bd545
Ill_cat <11940077+illcat74@user.noreply.gitee.com>
2025-07-08 22:25:14 +0800

    把补丁工具拉过来

---
+++ bsdiff/install.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001 > nul
+title mobmania 汉化补丁安装工具
+echo 正在安装汉化补丁...
+if not exist "data.win" (
+    echo 错误：请将此补丁放在游戏目录下（和 data.win 同目录）！
+    pause
+    exit
+)
+
+echo 备份原版 data.win...
+copy "data.win" "data.win.bak"
+
+echo 正在应用汉化补丁...
+bspatch.exe data.win data.win data.patch
+echo 汉化完成！按任意键退出...
+pause >nul
